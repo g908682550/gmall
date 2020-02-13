e045f5d93f6b3d257f7c561b9f94346a95ccc5b6
tom_glb <gy_glb@npu.com>
2020-02-13 21:46:22 +0800

    修改

--- gmall-pms/src/main/java/com/npu/gmall/pms/service/impl/ProductServiceImpl.java
+++ gmall-pms/src/main/java/com/npu/gmall/pms/service/impl/ProductServiceImpl.java
@@ -70,6 +70,23 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
     ThreadLocal<Long> threadLocal=new ThreadLocal<>();
 
     @Override
+    public PageInfoVo productPageInfo(PmsProductQueryParam param) {
+
+        QueryWrapper<Product> wrapper=new QueryWrapper<>();
+
+        if(param.getBrandId()!=null) wrapper.eq("brand_id",param.getBrandId());
+        if(!StringUtils.isEmpty(param.getKeyword())) wrapper.like("name",param.getKeyword());
+        if(param.getProductCategoryId()!=null) wrapper.eq("product_category_id",param.getProductCategoryId());
+        if(!StringUtils.isEmpty(param.getProductSn())) wrapper.like("product_sn",param.getProductSn());
+        if(param.getPublishStatus()!=null) wrapper.eq("publish_status",param.getPublishStatus());
+        if(param.getVerifyStatus()!=null) wrapper.eq("verify_status",param.getVerifyStatus());
+
+        IPage<Product> page = productMapper.selectPage(new Page<Product>(param.getPageNum(), param.getPageSize()), wrapper);
+        PageInfoVo vo = new PageInfoVo(page.getTotal(), page.getPages(), param.getPageSize(), page.getRecords(), page.getCurrent());
+        return vo;
+    }
+
+    @Override
     public EsProduct productAllInfo(Long id) {
 
         EsProduct esProduct=null;
@@ -255,22 +272,6 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
         productMapper.updateById(product);
     }
 
-    @Override
-    public PageInfoVo productPageInfo(PmsProductQueryParam param) {
-
-        QueryWrapper<Product> wrapper=new QueryWrapper<>();
-
-        if(param.getBrandId()!=null) wrapper.eq("brand_id",param.getBrandId());
-        if(!StringUtils.isEmpty(param.getKeyword())) wrapper.like("name",param.getKeyword());
-        if(param.getProductCategoryId()!=null) wrapper.eq("product_category_id",param.getProductCategoryId());
-        if(!StringUtils.isEmpty(param.getProductSn())) wrapper.like("product_sn",param.getProductSn());
-        if(param.getPublishStatus()!=null) wrapper.eq("publish_status",param.getPublishStatus());
-        if(param.getVerifyStatus()!=null) wrapper.eq("verify_status",param.getVerifyStatus());
-
-        IPage<Product> page = productMapper.selectPage(new Page<Product>(param.getPageNum(), param.getPageSize()), wrapper);
-        PageInfoVo vo = new PageInfoVo(page.getTotal(), page.getPages(), param.getPageSize(), page.getRecords(), page.getCurrent());
-        return vo;
-    }
 
     /**
      * 大保存
---
+++ gmall-shop-portal/src/main/java/com/npu/gmall/portal/controller/MemberController.java
@@ -0,0 +1,68 @@
+package com.npu.gmall.portal.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.npu.gmall.constant.SysCacheConstant;
+import com.npu.gmall.to.CommonResult;
+import com.npu.gmall.ums.entity.Member;
+import com.npu.gmall.ums.service.MemberService;
+import com.npu.gmall.vo.ums.LoginResponseVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Reference
+    MemberService memberService;
+
+    @PostMapping("/applogin")
+    @ResponseBody
+    public CommonResult loginForGmall(@RequestParam("username") String username,
+                                      @RequestParam("password") String password){
+
+        Member member=memberService.login(username,password);
+        if(member==null){
+            //用户没有
+            CommonResult commonResult = new CommonResult().failed();
+            commonResult.setMessage("账号密码不匹配，请重新登录");
+            return commonResult;
+        }else{
+            String token = UUID.randomUUID().toString().replace("-","");
+            String memberJson = JSON.toJSONString(member);
+            stringRedisTemplate.opsForValue()
+                    .set(SysCacheConstant.LOGIN_MEMBER+token,memberJson,SysCacheConstant.LOGIN_MEMBER_TIMEOUT, TimeUnit.MINUTES);
+            LoginResponseVo loginResponseVo = new LoginResponseVo();
+            BeanUtils.copyProperties(member,loginResponseVo);
+            //设置访问令牌
+            loginResponseVo.setAccessToken(token);
+            return new CommonResult().success(loginResponseVo);
+        }
+    }
+
+    @ResponseBody
+    @GetMapping("/userinfo")
+    public CommonResult getUserInfo(@RequestParam("accessToken") String token){
+
+        String redisKey=SysCacheConstant.LOGIN_MEMBER+token;
+
+        String member = stringRedisTemplate.opsForValue().get(redisKey);
+
+        Member loginMember = JSON.parseObject(member, Member.class);
+
+        loginMember.setId(null);
+        loginMember.setPassword(null);
+
+        return new CommonResult().success(loginMember);
+    }
+
+}
